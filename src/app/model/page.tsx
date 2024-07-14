40eb92b52e2102c43e90320464e19ee6738cf28f
DeHopen <a2302004b23@yandex.ru>
2024-07-14 19:20:37 +0300

    Абдейт большой и исправлени багов

--- src/app/model/page.tsx
+++ src/app/model/page.tsx
@@ -1,10 +1,11 @@
 import CommunicationWindow from "@/components/ModelPage/ModelPage";
+import {Suspense} from "react";
 
 
 export default function Model() {
   return (
-      <div>
-        <CommunicationWindow/>
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <CommunicationWindow />
+      </Suspense>
   );
 }
